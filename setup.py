73b13a89ca3732d785509fe6aca9dba0b7b8a5de
Jacob Magnusson <m@jacobian.se>
2019-07-30 13:21:47 +0200

    v0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup_kwargs = dict(
     name='sentry_dramatiq',
-    version='0.3.0',
+    version='0.3.1',
     description='Dramatiq task processor integration for the Sentry SDK',
     long_description=long_description,
     long_description_content_type='text/markdown',
